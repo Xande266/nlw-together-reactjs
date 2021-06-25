53b0c34492aa9699d99310931cae02f12a0e3915
TheSplit <fabiocaldas266@gmail.com>
2021-06-24 21:39:40 -0300

    0.0.1

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+import { AuthContextProvide } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <AuthContextProvide>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room}></Route>
+            <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
+          </Switch>
+        </AuthContextProvide>
+    </BrowserRouter>
+  );
+}
+
+export default App;
